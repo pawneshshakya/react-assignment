4895e5e56dda84904ae231ba7c7898faefbc69b1
Pawnesh Shakya <pawneshkumar162@gmail.com>
2023-03-26 15:33:40 +0530

    fixed design

--- src/creditcards/Cards.jsx
+++ src/creditcards/Cards.jsx
@@ -9,7 +9,7 @@ import "react-stacked-carousel/dist/index.css";
 const Cards = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-//   const [cardList, setCardList] = useState([]);
+  //   const [cardList, setCardList] = useState([]);
   const [currentIndex, setCurrentIndex] = useState(0);
   const [page, setPage] = useState(1);
   const { cards: cardData, cardList } = useSelector((state) => state.cards);
@@ -27,37 +27,35 @@ const Cards = () => {
     }
   };
   return (
-    <div className="main-container w-100 ">
+    <div className="w-100 ">
       <button type="button" class="btn btn-secondary" onClick={handleClick}>
         add new card
       </button>
-      <div className="card-stack">
-        <StackedCarousel
-          autoRotate={false}
-          onCardChange={onCardChange}
-          containerClassName={""}
-          cardClassName=""
-          leftButton={<button disabled={currentIndex === 0}>{"<"}</button>}
-          rightButton={
-            <button disabled={currentIndex === cardData?.totalResults}>
-              {">"}
-            </button>
-          }
-        >
-          {cardData?.totalResults > 0 &&
-            cardList?.map((card, i) => {
-              return (
-                <Card
-                  key={i}
-                  number={card?.cardNumber}
-                  name={card?.cardHolder}
-                  expiry={card?.cardExpiration}
-                  cvc={card?.cvc}
-                />
-              );
-            })}
-        </StackedCarousel>
-      </div>
+      <StackedCarousel
+        autoRotate={false}
+        onCardChange={onCardChange}
+        containerClassName={""}
+        cardClassName=""
+        leftButton={<button disabled={currentIndex === 0}>{"<"}</button>}
+        rightButton={
+          <button disabled={currentIndex === cardData?.totalResults}>
+            {">"}
+          </button>
+        }
+      >
+        {cardData?.totalResults > 0 &&
+          cardList?.map((card, i) => {
+            return (
+              <Card
+                key={i}
+                number={card?.cardNumber}
+                name={card?.cardHolder}
+                expiry={card?.cardExpiration}
+                cvc={card?.cvc}
+              />
+            );
+          })}
+      </StackedCarousel>
     </div>
   );
 };
